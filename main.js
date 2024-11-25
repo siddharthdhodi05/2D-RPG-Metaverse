8370b559587c4b4893c9965d2f06575ee2fc5f09
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-11-26 00:20:45 +0530

    Collision Feature

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-import { GameLoop } from './src/GameLoop';
-import { resourses } from './src/Resource';
-import { Sprite } from './src/Sprite';
-import { Vector2 } from './src/Vector2';
-import './style.css';
+import { GameLoop } from "./src/GameLoop";
+import { gridCells, isSpaceFree } from "./src/helpers/grid";
+import { moveTowards } from "./src/helpers/moveTowards";
+import { DOWN, Input, LEFT, RIGHT, UP } from "./src/Input";
+import { walls } from "./src/levels/level1";
+import { resourses } from "./src/Resource";
+import { Sprite } from "./src/Sprite";
+import { Vector2 } from "./src/Vector2";
+import "./style.css";
 
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
@@ -23,9 +27,12 @@ const hero = new Sprite({
   hFrames: 3,
   vFrames: 8,
   frame: 0, // Start from frame 0
+  position: new Vector2(gridCells(6), gridCells(5)),
 });
 
-const heroPos = new Vector2(16 * 6, 16 * 5);
+const heroDestinationPosition = hero.position.duplicate();
+
+const input = new Input();
 
 const shadow = new Sprite({
   resource: resourses.images.shadow,
@@ -33,9 +40,45 @@ const shadow = new Sprite({
 });
 
 const update = () => {
+  const distance = moveTowards(hero, heroDestinationPosition, 1);
+  const hasArrived = distance <=1;
+  //attempt to move again if the hero is at the position
+  if(hasArrived){
+    tryMove()
+  }
+};
 
-  hero.frame +=1 //(hero.frame + 1) % (hero.hFrames * hero.vFrames); // Increment and wrap frame
-  //heroPos.x+=1
+const tryMove = () => {
+  if (!input.direction){
+    return;
+  }
+
+  let nextX = heroDestinationPosition.x
+  let nextY = heroDestinationPosition.y
+  const gridSize = 16; 
+
+  if (input.direction === DOWN) {
+    nextY += gridSize;
+    hero.frame = 0;
+  }
+  if (input.direction === UP) {
+    nextY -= gridSize;
+    hero.frame = 6;
+  }
+  if (input.direction === LEFT) {
+    nextX -= gridSize;
+    hero.frame = 9;
+  }
+  if (input.direction === RIGHT) {
+    nextX += gridSize;
+    hero.frame = 3;
+  }
+  
+  if(isSpaceFree(walls, nextX, nextY)){
+    heroDestinationPosition.x = nextX
+    heroDestinationPosition.y = nextY
+  }
+  
 };
 
 const draw = () => {
@@ -43,8 +86,8 @@ const draw = () => {
   groundSprite.drawImage(ctx, 0, 0);
 
   const heroOffset = new Vector2(-8, -21);
-  const heroPosX = heroPos.x + heroOffset.x;
-  const heroPosY = heroPos.y + 1 + heroOffset.y;
+  const heroPosX = hero.position.x + heroOffset.x;
+  const heroPosY = hero.position.y + 1 + heroOffset.y;
 
   shadow.drawImage(ctx, heroPosX, heroPosY);
   hero.drawImage(ctx, heroPosX, heroPosY); // Use hero instead of heroSprite
